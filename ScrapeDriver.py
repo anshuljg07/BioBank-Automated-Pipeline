cc2d8ef0be81433ee573e5d9764fede22e44596f
anshuljg07 <anshuljg07@gmail.com>
2022-06-08 14:54:30 -0400

    Scrape Driver to process files

--- ScrapeDriver.py
+++ ScrapeDriver.py
@@ -1,10 +1,11 @@
 import os
 
 
-class DrivePatch:
+class ScrapeDriver:
     def __init__(self):
         self.drivepath = '/Volumes/MoledinaLab-CC1032-MEDINT/Biobank 27890/Pathology Report PDF/'
         self.homepath = '/Users/anshulgowda/Documents/CODE/KUH2022/'
+        self.pdfs = []
 
     def CreatePatch(self):
         try:
@@ -28,18 +29,31 @@ class DrivePatch:
 
     def PullFiles(self):
         if(self.CreatePatch()):  # create patch to share drive
-            pass
+            pdflist = os.listdir(os.getcwd())
+            # return list of full paths
+            self.pdfs = [self.homepath + filename for filename in pdflist]
+
+        else:
+            return None
+    def
 
 
 def main():
+    Drive = DrivePatch()
+
+    homepath = '/Users/anshulgowda/Documents/CODE/KUH2022/'
     print('working directory: {}'.format(os.getcwd()))
     # os.chdir('/Volumes/MoledinaLab-CC1032-MEDINT/Biobank 27890/Pathology Report PDF/')
     os.chdir('/Users/anshulgowda/Documents/CODE/KUH2022/')
     print('working directory: {}'.format(os.getcwd()))
 
-    for filename in os.listdir(os.getcwd()):
-        # call the read pdf funcs in "jpegtotext.py" prolly need to rename the file to
-        # "biopsyscraper.py"
+    pdflist = os.listdir(os.getcwd())
+    for j in [homepath + i for i in pdflist]:
+        print('\n\n{}\n\n'.format(j))
+
+    # for filename in os.listdir(os.getcwd()):
+    # call the read pdf funcs in "jpegtotext.py" prolly need to rename the file to
+    # "biopsyscraper.py"
 
 
 main()
