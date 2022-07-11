432693d4a910b7cd3424502d43b0c6e069a7485c
anshuljg07 <anshuljg07@gmail.com>
2022-07-11 15:49:33 -0400

    finished? with OOP CUIsearch() w/NER recurs. solution. Untested

--- SciNLP.py
+++ SciNLP.py
@@ -11,25 +11,18 @@ from itertools import chain, combinations, permutations
 
 
 '''
-Object to represent the kb matches in a document. Attributes include the unique CUI, entity.text, Canonical name, USML definition,
+Object to represent the matches in a document. Attributes include the unique CUI, entity.text, Canonical name, USML definition,
 and the dependency information, which includes the head and children of the given tokens
 '''
 
 
-class CUIgroup:
-    # def __init__(self, cui, canon, definition, family, negation):
-    def __init__(self, cui, canon, definition, family):
+class Group:
+    def __init__(self, cui, entity, canon, definition, family):
         self.cui = cui
         self.entity = entity
         self.canon = canon
         self.definition = definition
-        self.family = family  # contains a list of the head/children for each token in the entity
-        # contain a dictionary of inner CUIs and their matching text query
-        self.innercuis = {}  # ex: {}
-        # hello
-        # self.head = head
-        # self.children = children
-        # self.negation = negation #to be added soon once how to do negation is figured out
+        self.family = family
 
     '''
     Use Negspacy to see if the individual tokens are negated or carry a negation token.
@@ -40,27 +33,95 @@ class CUIgroup:
 
 
 '''
-Object meant to hold the CUIgroup and organize them based on the sections from which they were derived.
+Object that corresponds to a Named Entity using the Spacy Named Entity Recognizer (NER). The named entity is generated
+using Spacy's NLP algorithim trained on clinical data. This entity is then searched for in the USML (KB || Knowledge Base),
+and returns a named entity match if it exists. This named entity match gets a unique ID (CUI) and is given a clinical umbrella
+term (canon || canonical name), and if it exists an associated medical definition. If matches are not found the named entity is
+checked for gramatical dependency tokens, which are stored in family. Using Negspacy, an algorithim that detects for negation,
+negation is noted with relation to the named entity.
+'''
+
+
+class CUIgroup(Group):
+    # def __init__(self, cui, canon, definition, family, negation):
+    def __init__(self, cui, entity, canon, definition, family, negation):
+        super().__init__(cui, entity, canon, definition, family)
+        self.subCUIgroups = []  # store the subCUIgroup objects
+        self.subCUIs = []  # store the CUIs (vals)
+        self.containSubs = False
+
+        # self.cui = cui
+        # self.entity = entity
+        # self.canon = canon
+        # self.definition = definition
+        # self.family = family
+        # self.subcuiexists = False  # contains a list of the head/children for each token in the entity
+        # contain a dictionary of inner CUIs and their matching text query
+        # hello
+        # self.head = head
+        # self.children = children
+        # self.negation = negation #to be added soon once how to do negation is figured out
+
+    def addsubCUI(self, subCUI, subCUIgroup):
+        if(not self.containSubs):
+            self.containSubs = True
+        self.subCUIs.append(subCUI)
+        self.subCUIgroups.append(subCUIgroup)
+
+    def find_negation(self):  # keep in parent class? or keep in the children?
+        pass
+
+
+'''
+Object meant to be the subCUIs inside of the larger CUIgroup, multiple subCUIs may exist for a CUIgroup that resulted
+in no matches in the KB. Therefore a NULL valued CUIgroup may have multiple non-NULL-valued subCUI groups
+'''
+
+
+class subCUIgroup(Group):
+    # should I store the text, the entity, or the token?
+    def __init__(self, cui, entity, canon, definition, family):
+        super().__init__(cui, entity, canon, definition, family)
+
+
+'''
+Object meant to hold the text of an individual section dervied from the ScrapeDriver. Contains an Analyze() function to
+use the Spacy's Named Entity Recognizer (NER), which uses a machine learned model trained on clinical data to recognize
+common clinical entities. These are then cross referenced with the USML {word for collection of words}, and matches are found.
 '''
 
 
 class Section:
     def __init__(self, text):
+        self.fmt_str = "{:<20} | {:<10} | {:<20} | {:<40}"
+        self.sub_fmt_str = "\t{:<15} ~ {:<15} ~ {:<35}"
+        self.query_fmt = "{:<20} $ {:<20} $ {:<20}"
+        self.donerecursion = False
         self.text = text
         self.cuis = []
         self.cuigroups = []
 
+        # testing viability
+        self.tempNullValCUI = None
+
     def __repr__(self):
-        fmt_str = "{:<20} | {:<10} | {:<20} | {:<40}"
-        print(fmt_str.format("Entity", "1st CUI", "Canonical Name", "Definition"))
+        print(self.fmt_str.format("Entity", "1st CUI", "Canonical Name", "Definition"))
         for group in cuigroups:
-            print(fmt_str.format(group.entity.text, group.cui, group.canon, group.definition))
+            if(not group.subcuiexists):  # if no subcuis exists
+                print(fmt_str.format(group.entity.text, group.cui, group.canon, group.definition))
+            else:
+                # print out the subcui entities
 
     def Analyze(self, nlp, linker):
         doc = nlp(self.text)
         for entity in doc.ents:
+            self.donerecursion = False
+            self.tempNullValCUI = None
             CUIsearch_helper(entity, nlp, linker)
+            self.cuigroups.append(self.tempNullValCUI)
 
+    # add variable for recursion ('recurs')
+    # make linker a class attribute??
     def CUIsearch_helper(self, entity, nlp, linker):
         if(len(entity._.kb_ents) != 0):  # match exists in kb
             first_cuid = entity._.kb_ents[0][0]  # CAN I REPLACE "CUID" with "CUI"
@@ -80,40 +141,143 @@ class Section:
                         match_score = kb_entry[1]
                         continue
                     else:  # found a match with a Definition
-                        if(entity.text.split() > 1):
-                            family = find_HeadandChildren(entity.text, nlp)
+                        # no match was found originally, so create null valued CUI group (None for family). Then link it to
+                        # the null valued CUIgroup
+                        if(self.donerecursion and self.tempNullValCUI != None):
+                            subgroup = subCUIgroup(
+                                cui, entity, query.canonical_name, query.definition, None)
+
+                            # link to the null valued CUI group
+                            self.tempNullValCUI.subCUIgroups.append(subgroup)
+                            self.tempNullValCUI.subCUIs.append(cui)
+                            self.cuis.append(cui)
+                            return
                         else:
-                            family = []
-                        tempCUI = CUIgroup(cui, query.canonical_name, query.definition, family)
-                        self.cuigroups.append(tempCUI)  # add cui grouping to the list of groups
-                        self.cuis.append(cui)  # add unique cui to the list of cuis
-                        return
+                            family = find_HeadandChildren(entity.text, nlp)
+                            tempCUI = CUIgroup(cui, entity, query.canonical_name,
+                                               query.definition, family)
+                            self.cuigroups.append(tempCUI)  # add cui grouping to the list of groups
+                            self.cuis.append(cui)  # add unique cui to the list of cuis
+                            print(self.fmt_str.format(entity.text, cui,
+                                                      query.canonical_name, query.definition))
+                            return
+
                 # if for loop completes then no match with a definition was found, so use the first match (best match)
-                if(entity.text.split() > 1):
-                    family = find_HeadandChildren(entity.text, nlp)
+                if(self.donerecursion and self.tempNullValCUI != None):
+                    subgroup = subCUIgroup(first_cuid, entity, query.canonical_name, '', None)
+
+                    # link to the null valued CUI group
+                    self.tempNullValCUI.subCUIgroups.append(subgroup)
+                    self.tempNullValCUI.subCUIs.append(first_cuid)
+                    self.cuis.append(first_cuid)
+                    return
                 else:
-                    family = []
-                tempCUI = CUIgroup(first_cuid, query.canonical_name, '', family)
-                self.cuigroups.append(tempCUI)
-                self.cuis.append(first_cuid)
+                    family = find_HeadandChildren(entity.text, nlp)
+                    tempCUI = CUIgroup(first_cuid, entity, query.canonical_name, '', family)
+                    self.cuigroups.append(tempCUI)
+                    self.cuis.append(first_cuid)
+                    print(self.fmt_str.format(entity.text, first_cuid, query.canonical_name, ''))
+                    return
 
             # if the first match contains a definition use the first match
             else:
-                if(entity.text.split() > 1):
-                    family = find_HeadandChildren(entity.text, nlp)
-                else:
-                    family = []
+                if(self.donerecursion and self.tempNullValCUI != None):
+                    subgroup = subCUIgroup(
+                        first_cuid, entity, query.canonical_name, query.definition, None)
 
-                # since no def exists, pass empty string
-                # possible that .canonical_name could return NoneType as well
-                tempCUI = CUIgroup(first_cuid, query.canonical_name, query.definition, family)
-                self.cuigroups.append(tempCUI)
-                self.cuis.append(first_cuid)
+                    # link to the null valued CUI group
+                    self.tempNullValCUI.subCUIgroups.append(subgroup)
+                    self.tempNullValCUI.subCUIs.append(first_cuid)
+                    self.cuis.append(first_cuid)
+                    return
+
+                else:
+                    family = find_HeadandChildren(entity.text, nlp)
+                    # since no def exists, pass empty string
+                    # possible that .canonical_name could return NoneType as well
+                    tempCUI = CUIgroup(first_cuid, entity, query.canonical_name,
+                                       query.definition, family)
+                    self.cuigroups.append(tempCUI)
+                    self.cuis.append(first_cuid)
 
         else:  # match doesn't exist in kb
-            # find combinations of the head + children and search for them in the KB.
+            if(self.donerecursion == True):
+                return
+            # family = find_HeadandChildren(entity.text, nlp)
+            # noCUI = CUIgroup(None, entity, None, None, family) #create a null valued CUIgroup
+            # self.cuigroups.append(noCUI)
 
-            pass
+            # CHECKING VIABILITY
+            family = find_HeadandChildren(entity.text, nlp)
+            self.tempNullValCUI = CUIgroup(None, entity, None, None, family)
+            self.tempNullValCUI.subcuiexists = True
+
+            # add NullValCUI at the end or at the beginning? BEGNINNING OPTION
+            # self.cuigroups.append(self.tempNullValCUI)
+
+            # use the Spacy tokenizer to analyze dependencies
+            if(len(family > 0)):
+                subtext = entity.text
+                sub_doc = nlp(subtext)
+                heads = []            # contain the heads of the text
+                children = []         # contain the children of the text
+                head_combos = []      # contain all possible combinations/permutations of the heads
+                child_combos = []     # contain all possible combinations/permutations of the children
+
+                for token in sub_doc:
+                    if(len(list(token.children)) != 0):  # if the token contains children, then the token is a head
+                        if(token.head.text not in heads):
+                            heads.append(token.head.text)
+
+                            # add the children to the children list, if they do not already exist, non list comp. version below
+                            [children.append(i.text)
+                             for i in list(token.children) if i.text not in children]
+                            # for i in list(token.children):
+                            #     children.append(i.text) #add the head's children to the
+
+                # generates all possible combinations of heads from size (0 -> n)
+                head_combos = list(chain.from_iterable(permutations(heads, r)
+                                                       for r in range(len(heads)+1)))
+
+                # generates a powerset of all possible combinations of heads for those of size (1 -> n-1)
+                # head_combos = list(chain.from_iterable(combinations(heads, r) for r in range(len(heads)+1)))
+
+                # generates all possible combinations of children from size (0 -> n)
+                child_combos = list(chain.from_iterable(permutations(children, r)
+                                                        for r in range(len(children)+1)))
+                # generates a powerset of all possible combinations of heads for those of size (1 -> n-1)
+                # child_combos = list(chain.from_iterable(combinations(children, r) for r in range(len(children)+1)))
+
+                head_combos = list(head_combos)
+                child_combos = list(child_combos)
+
+                print(head_combos)
+                print(child_combos)
+
+                for i, headcombo in enumerate(head_combos):
+                    if(len(headcombo) == 0):
+                        continue
+                    for j, childrencombo in enumerate(child_combos):
+                        if(len(childrencombo) == 0):
+                            continue
+                    # generate subquery string, that will be fed to the KB
+                    subquery = (' '.join(childrencombo)) + ' ' + (' '.join(headcombo))
+                    print(query_fmt.format(' '.join(childrencombo), ' '.join(headcombo), subquery))
+
+                    # if you're simply searching for the original entity, skip it
+                    if subquery == entity.text:
+                        continue
+
+                    # run entity recognizer on the subquery
+                    innerdoc = nlp(subquery)
+                    for innerentity in innerdoc.ents:
+                        # handle recursion
+                        self.donerecursion = True
+                        CUIsearch_helper(self, innerentity, nlp, linker)
+
+            # print(fmt_str.format(entity.text, 'xxxxxxxx', 'NO ENTRY FOUND', 'NO DEF'))
+
+            # find combinations of the head + children and search for them in the KB.
             # takes the subtext and finds the head/children of them and returns them to be loaded into the CUIgroup's head and
             # children attributes
 
@@ -122,11 +286,14 @@ class Section:
     '''
 
     def find_HeadandChildren(self, subtext, nlp):
-        familycollection = []
-        sub_doc = nlp(subtext)
-        for token in sub_doc:
-            familycollection.append([token.text, token.head.text, list(token.children)])
-        return familycollection
+        if(subtext.split() > 1):
+            familycollection = []
+            sub_doc = nlp(subtext)
+            for token in sub_doc:
+                familycollection.append([token.text, token.head.text, list(token.children)])
+            return familycollection
+        else:
+            return []
 
 
 class NLPDriver():
@@ -193,9 +360,6 @@ def CUIsearch(entity, nlp, fmt_str, recurs):
                 heads.append(token.head.text)
                 for i in list(token.children):
                     children.append(i.text)
-                # print(type(list(token.children)[0].text))
-                # children.extend(list(token.children))
-                # print(children)
 
         # once all the heads and tokens are fxound, use the algo
         # for r in range(len(heads)+1):
@@ -247,7 +411,7 @@ def CUIsearch(entity, nlp, fmt_str, recurs):
                 if subquery == entity.text:
                     continue
 
-                # run entity recognizer
+                # run entity recognizer on the subquery
                 innerdoc = nlp(subquery)
                 for innerentity in innerdoc.ents:
                     recurs = True
